38c0c08b7d89c4eb29412bd74a1cb145092d34ee
MarsilAL <alashkar.marsil@gmail.com>
2020-05-28 13:53:40 +0200

    Updates

---
+++ precache-manifest.5cfaa600147aed0c4c3c489e39b59e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162b8f41659e890618210950a0a76a17",
+    "url": "/azubi-todo-app/index.html"
+  },
+  {
+    "revision": "09eb6ece3a4e4e547697",
+    "url": "/azubi-todo-app/static/css/main.a97c9717.chunk.css"
+  },
+  {
+    "revision": "94e2d336f260744a3f49",
+    "url": "/azubi-todo-app/static/js/2.a4badb7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/azubi-todo-app/static/js/2.a4badb7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09eb6ece3a4e4e547697",
+    "url": "/azubi-todo-app/static/js/main.7c139d17.chunk.js"
+  },
+  {
+    "revision": "a418e38c14a01ddfe125",
+    "url": "/azubi-todo-app/static/js/runtime-main.0295eccc.js"
+  }
+]);
